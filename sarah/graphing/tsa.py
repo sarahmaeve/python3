2b0e8c11b91f444d27f119a2cdd6216110de7eb9
Sarah <sarahmaeve@users.noreply.github.com>
2023-03-15 11:22:34 -0700

    Updated TSA for 2023

--- sarah/graphing/tsa.py
+++ sarah/graphing/tsa.py
@@ -3,38 +3,43 @@ import numpy as np
 import requests
 import matplotlib.pyplot as plt
 
-# Pandas test to reformat TSA passenger data into a more usable format.
+def main():
+    # TSA official passenger data
+    tsa_url = 'https://www.tsa.gov/coronavirus/passenger-throughput'
+    header = {
+      "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+      "X-Requested-With": "XMLHttpRequest"
+    }
 
-# impersonate headers to avoid 403 error
-# TSA official passenger data
-tsa_url = 'https://www.tsa.gov/coronavirus/passenger-throughput'
-header = {
-  "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
-  "X-Requested-With": "XMLHttpRequest"
-}
+    r = requests.get(tsa_url, headers=header)
 
-r = requests.get(tsa_url, headers=header)
+    table_tsa = pd.read_html(r.text)
 
-table_tsa =  pd.read_html(r.text)
+    print(f'Total tables: {len(table_tsa)}')
 
-print(f'Total tables: {len(table_tsa)}')
+    df = table_tsa[0]
 
-df = table_tsa[0]
+    # strip the year so it's easier to do comparisons
+    df['Date'] = df['Date'].astype('datetime64').dt.strftime('%m/%d')
+    df['2023'] = df['2023'].astype('float64')
+    df['2022'] = df['2022'].astype('float64')
+    df['2021'] = df['2021'].astype('float64')
+    df['2020'] = df['2020'].astype('float64')
+    df['2019'] = df['2019'].astype('float64')
 
-# strip the year so it's easier to do comparisons
-df['Date'] = df['Date'].astype('datetime64').dt.strftime('%m/%d')
-df['2022'].astype('float64')
+    df.set_index('Date', inplace=True)
+    df.sort_index(inplace=True)
 
-# can move this into data formatting during the read_html() call?
-df['2021'].astype('float64')
-df['2020'].astype('float64')
-df['2019'].astype('float64')
+    plot_tsa_data(df)
 
-df.sort_values(by='Date',inplace=True)
+def plot_tsa_data(df):
+    plt.style.use('seaborn-v0_8-whitegrid')
+    df.plot.line(y=['2019', '2020', '2021', '2022','2023'])
+    plt.title('TSA Passenger Data (2019: no COVID-19)')
+    plt.xlabel('Day of Year')
+    plt.ylabel('Passengers (Millions)')
+    plt.show()
 
-plt.style.use('seaborn-whitegrid')
-df.plot.line(x='Date', y=['2019','2020','2021','2022'])
-plt.title('TSA Passenger Data (2019 : no COVID-19)')
-plt.xlabel('Day of Year')
-plt.ylabel('Passengers (Millions)')
-plt.show()
+if __name__ == "__main__":
+    main()
+
